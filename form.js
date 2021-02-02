9e0b20f3585f464abfcc90cc548f47017f920ae3
Batool H <batool.a.hasan@gmail.com>
2021-02-02 10:13:29 +0300

    Add files via upload

--- form.js
+++ form.js
@@ -5,7 +5,8 @@ const error1= "Whoops! It looks like you forgot to add your email";
 const error2= "Please provide a valid email address";
 
 
-formButton.addEventListener('click', ()=>{
+formButton.addEventListener('click', (evt)=>{
+    evt.preventDefault();
     re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!formInput.value.match(re) || !formInput.value){
         formLabel.innerHTML=error2;
